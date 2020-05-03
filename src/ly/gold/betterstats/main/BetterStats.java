232bed6965d009eab3a06246559c89093e477718
Ge <55292128+alpharout@users.noreply.github.com>
2020-05-03 11:22:44 +0200

    Pre-Release [2] - Added config for mysql data

--- src/ly/gold/betterstats/main/BetterStats.java
+++ src/ly/gold/betterstats/main/BetterStats.java
@@ -4,13 +4,28 @@ import ly.gold.betterstats.api.Data;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BetterStats extends JavaPlugin {
+    private static BetterStats plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
+
         Data.log("§7BetterStats is started!");
+
+        this.getConfig().addDefault("mysql.hostname", "localhost");
+        this.getConfig().addDefault("mysql.database", "testbase");
+        this.getConfig().addDefault("mysql.username", "admin");
+        this.getConfig().addDefault("mysql.password", "verysecretpassword");
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
     }
 
     @Override
     public void onDisable() {
         Data.log("§7BetterStats is stopped!");
+    }
+
+    public static BetterStats getInstance() {
+        return plugin;
     }
 }
